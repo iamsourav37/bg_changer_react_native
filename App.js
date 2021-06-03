09ae6e8ba3d0f382c31241833f136d187ac05b74
iamsourav37 <souravganguly108@gmail.com>
2021-06-03 15:32:29 +0530

    reset button added

--- App.js
+++ App.js
@@ -11,11 +11,18 @@ const App = () => {
     console.log(`generated COLOR: rgb(${first},${second},${third})`);
     setRandomColor(`rgb(${first},${second},${third})`);
   };
+  const resetDefault = () => {
+    setRandomColor('rgb(32, 0, 126)');
+  };
   return (
     <>
       <View style={[styles.viewStyle, {backgroundColor: randomColor}]}>
         <TouchableOpacity onPress={changeBgColor}>
           <Text style={styles.textStyle}>BG CHANGER</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={resetDefault}>
+          <Text style={styles.resetStyle}>RESET</Text>
         </TouchableOpacity>
       </View>
     </>
@@ -33,9 +40,18 @@ const styles = StyleSheet.create({
   textStyle: {
     color: '#fff',
     fontSize: 30,
-    backgroundColor: '#bb2c39',
+    backgroundColor: '#B4161B',
     paddingVertical: 10,
     paddingHorizontal: 40,
+    borderRadius: 15,
+  },
+  resetStyle: {
+    color: '#fff',
+    fontSize: 25,
+    backgroundColor: '#23C4ED',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    marginTop: 10,
     borderRadius: 15,
   },
 });
